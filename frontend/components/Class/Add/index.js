d415b986139aba26fc664c1c4873b3c6f1ebbf0c
Yury Shevchenko <shevchenko_yury@mail.ru>
2020-03-09 15:57:06 +0100

    onboarding survey

--- frontend/components/Class/Add/index.js
+++ frontend/components/Class/Add/index.js
@@ -35,7 +35,7 @@ class AddClass extends Component {
     const { name, type, value } = e.target;
     const val = type === 'number' ? parseFloat(value) : value;
     this.setState({
-      [name]: value,
+      [name]: val,
     });
   };
 
@@ -109,6 +109,7 @@ class AddClass extends Component {
                   required
                 />
               </label>
+
               <label htmlFor="description">
                 Description
                 <textarea
